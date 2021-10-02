17945bdfc76dec6f2376547af5e50e07ac86a931
Kygan <kygandao23@gmail.com>
2021-10-02 09:54:52 -0700

    reformatting

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,12 +7,12 @@ const Homepage = () => {
                 <h1>Hello</h1>
             </div>
             <div className='homepageContentDiv'>
-                <h2>Welcome to my working portfolio.</h2>
                 <div className='homeLeft'>
 
                 </div>
                 
                 <div className='homeMiddle'>
+                    <h2>Welcome to my working portfolio.</h2>
                     <p className='homeBody'>My name is Kygan Dao (Key-gan Dow).<br/> I am a purpose driven Full Stack Developer with a background in Insurance, Utilities, and Visual Arts experience with a create approach. Eager to provide alternative perspectives to the challenges that await and embrace the unknown to expand my proficiencies. <br/>By formulating successful strategies, each project I contribute results in the strengthening of the concept as well as a polished end product. Adaptability, balanced view, and clear communication are the defining attributes that will be added to each team and be conveyed in each project.
                     </p>
                 </div>
