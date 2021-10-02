23e74da2eab0f0098940ac4a172e09d37259b005
Kygan <kygandao23@gmail.com>
2021-10-01 20:36:17 -0700

    moving files, fixed links after Heroku deployment

--- src/components/partials/sliderData.js
+++ src/components/partials/sliderData.js
@@ -6,6 +6,6 @@ export const sliderData = [
         image: 'https://gcdn.pbrd.co/images/j8j9EbEkkhCg.png?o=1'
     },
     {
-        image: 'https://gcdn.pbrd.co/images/eq9jGqblir7t.png?o=1'
+        image: 'https://gcdn.pbrd.co/images/c2riYxUwOcnz.png?o=1'
     }
 ]
