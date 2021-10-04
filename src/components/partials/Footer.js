230c41d350aba8db39dd5a668c9392a39918427e
Kygan <kygandao23@gmail.com>
2021-10-04 14:46:19 -0700

    added additional footer content

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import githubIcon from '../../imgs/githubicon.png'
 import linkedinIcon from '../../imgs/linkedinicon.png'
+import instagramIcon from '../../imgs/instagramicon.png'
 
 function FooterDiv() {
     return (
@@ -9,6 +10,8 @@ function FooterDiv() {
             <a href='https://github.com/Kygandao' target='_blank' rel='noreferrer'><img src={githubIcon} alt='Github Icon' className='icon'/></a>
 
             <a href='https://www.linkedin.com/in/kygan-dao-7a1893209/' target='_blank' rel='noreferrer'> <img src={linkedinIcon} alt='LinkedIn Icon' className='icon'/></a>
+
+            <img src={instagramIcon} alt='Instagram Icon' className='icon'/>
         </div>
         
     );
